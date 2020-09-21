4ae73b3d62989391d0e810056cb23b780790c904
Zam Montoya <zmontoya@phmlink.com>
2020-09-21 14:29:48 -0700

    WIP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment() === 'production') {
-            URL::forceScheme('https');
-        } else {
+        if (App::environment() === 'local') {
             return;
+        } else {
+            URL::forceScheme('https');
         }
     }
 }
