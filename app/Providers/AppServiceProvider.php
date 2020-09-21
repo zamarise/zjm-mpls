78bb84f7cf69852c7da3121b092b66a44be79cc8
Zam Montoya <zmontoya@phmlink.com>
2020-09-21 14:53:29 -0700

    Refactor; use https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment() == 'production') {
+        if (App::environment('production')) {
             URL::forceScheme('https');
         }
     }
