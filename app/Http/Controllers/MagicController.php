dc7dfe81a287ced6142baa10b687a6e885f455c4
Zam Montoya <zmontoya@phmlink.com>
2020-09-22 21:06:19 -0700

    Remove unnecessary imports

--- app/Http/Controllers/MagicController.php
+++ app/Http/Controllers/MagicController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Carbon\Carbon;
-use App\Models\User;
 use App\Models\Magic;
-use Carbon\Doctrine\CarbonDoctrineType;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
 
 class MagicController extends Controller
 {
